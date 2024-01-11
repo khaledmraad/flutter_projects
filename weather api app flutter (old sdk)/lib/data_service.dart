23ee1430a03b8708687c3f5e2b72f98c3d7c4ac3
Khaled Mrad <109146433+khaleedmrad@users.noreply.github.com>
2024-01-11 20:36:05 +0100

    initial commit

---
+++ weather api app flutter (old sdk)/lib/data_service.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:day11_weather_app/models.dart';
+import 'package:http/http.dart' as http;
+
+class DataService {
+  Future<WeatherResponse> getWeather(String city) async {
+    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+    final queryParameters = {'q': city, 'appid': 'KEYYY', 'units': 'imperial'};
+
+    final uri = Uri.https(
+        'api.openweathermap.org', '/data/2.5/weather', queryParameters);
+
+    final response = await http.get(uri);
+
+    print(response.body);
+    final json = jsonDecode(response.body);
+    return WeatherResponse.fromJson(json);
+  }
+}
